2308db2763fa38459a42fd7136a40ad22a6a6fd3
BalaGanesh5 <balaganesh405@gmail.com>
2020-09-16 21:40:31 +0530

    Updates

---
+++ precache-manifest.5fedc5c7776d28829499cda5d7ba2a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde80230d27a39ccf4600473af272d5f",
+    "url": "/sayings/index.html"
+  },
+  {
+    "revision": "9090cd3e5aa62f3bb553",
+    "url": "/sayings/static/css/main.f35a3551.chunk.css"
+  },
+  {
+    "revision": "c3decc5837b572a4379d",
+    "url": "/sayings/static/js/2.69b9263a.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/sayings/static/js/2.69b9263a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9090cd3e5aa62f3bb553",
+    "url": "/sayings/static/js/main.3f91fb57.chunk.js"
+  },
+  {
+    "revision": "e793b54d1d465ea674f8",
+    "url": "/sayings/static/js/runtime-main.88bdc081.js"
+  }
+]);
