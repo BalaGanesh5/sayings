2f8e8e94c2947b49608d0bed09dc06baf85ad482
BalaGanesh5 <balaganesh405@gmail.com>
2020-09-16 21:52:39 +0530

    updated state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import Card from "./Card/Card"
 import './App.css';
-import ParticlesBg from 'particles-bg'
+
 
 class App extends React.Component{
 
-  state = {advice:""}
+  state = {advice:" What ever the work may me, We have to give our best . . . "}
   
 
-componentDidMount(){
-  this.fetchAdvice();
-}
 
-fetchAdvice = async() => {
+
+fetchAdvice = () => {
   axios.get ('https://api.adviceslip.com/advice')
   .then((res)=> {
     const {advice} = res.data.slip;
